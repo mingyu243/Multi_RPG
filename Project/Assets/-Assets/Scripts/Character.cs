adc3db2bf665d88e3dc48ecbe9cf5bc9660149d4
mingyu243 <jmg6017@gmail.com>
2022-06-10 17:37:51 +0900

    플레이어 이동 스크립트 구현 중.

---
+++ Project/Assets/-Assets/Scripts/Character.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] float m_MoveSpeed = 3;
+    [SerializeField] float m_TurnSpeed = 360;
+
+    Rigidbody m_Rigidbody;
+    Animator m_Animator;
+    CapsuleCollider m_Capsule;
+
+    float h, v;
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Animator = GetComponent<Animator>();
+        m_Capsule = GetComponent<CapsuleCollider>();
+    }
+
+    public void Move(Vector3 move)
+    {
+        print(move.ToString());
+
+        // 이동.
+        Vector3 translation = move * m_MoveSpeed * Time.deltaTime;
+        transform.Translate(translation);
+
+        //// 회전.
+        //Vector3 euler = new Vector3(0, Mathf.Atan2(move.x, move.z) * m_TurnSpeed * Time.deltaTime, 0);
+        //transform.Rotate(euler);
+    }
+}
---
+++ Project/Assets/-Assets/Scripts/CharacterUserControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// 시네머신 관련.
+/// + https://www.youtube.com/watch?v=537B1kJp9YQ
+/// 
+/// 플레이어 이동 관련.
+/// + https://www.youtube.com/watch?v=b1uoLBp2I1w
+/// + Photon 데모 스크립트. (ThirdPersonUserControl, ThirdPersonCharacter)
+/// 
+/// </summary>
+public class CharacterUserControl : MonoBehaviour
+{
+    private Character m_Character;
+    private Transform m_Cam;
+
+    private Vector3 m_PlayerMovementInput;
+    private Vector3 m_PlayerMouseInput;
+
+    private void Start()
+    {
+        m_Cam = Camera.main.transform;
+        m_Character = GetComponent<Character>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        m_PlayerMovementInput = (v * m_Cam.forward) + (h * m_Cam.right);
+        m_PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+    }
+
+    private void FixedUpdate()
+    {
+        m_Character.Move(m_PlayerMovementInput);
+    }
+}

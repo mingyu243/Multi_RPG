dabf4a61ff2bab98c2a5a5361ab28913f28d5e3d
mingyu243 <79129407+mingyu243@users.noreply.github.com>
2022-08-11 00:49:46 +0900

    Basic Scene에 들어오면, 유저마다 캐릭터가 생성되도록 함. 이동은 잘 되는 듯. 애니메이션과 점프가 문제.

---
+++ Project/Assets/-Assets/Scripts/Flow/PlayerCreator.cs
@@ -0,0 +1,20 @@
+﻿using Cinemachine;
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCreator : MonoBehaviour
+{
+    [SerializeField] GameObject m_Camera;
+    [SerializeField] CinemachineVirtualCamera m_CVC;
+
+    void Start()
+    {
+        PlayerController pc = PhotonNetwork.Instantiate("PlayerController", Vector3.zero, Quaternion.identity).GetComponent<PlayerController>();
+        Character c = PhotonNetwork.Instantiate("Character", Vector3.zero, Quaternion.identity).GetComponent<Character>();
+
+        pc.SetCamera(m_Camera, m_CVC);
+        pc.OnPossess(c);
+    }
+}

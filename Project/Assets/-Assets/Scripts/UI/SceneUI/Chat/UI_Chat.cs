25ebe75b8c011385508e4bc17190cafd69ff1bcc
mingyu243 <jmg6017@gmail.com>
2022-11-21 20:30:51 +0900

    간단한 Chat 기능 구현 완료.
    - 엔터 누르면, Focus Chat UI.
    - 한번 더 누르면, Chat 내용 보내고 Focus 해제.

--- Project/Assets/-Assets/Scripts/UI/SceneUI/Chat/UI_Chat.cs
+++ Project/Assets/-Assets/Scripts/UI/SceneUI/Chat/UI_Chat.cs
@@ -1,8 +1,11 @@
+using ExitGames.Client.Photon;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class UI_Chat : UI_Base
 {
@@ -16,12 +19,22 @@ public class UI_Chat : UI_Base
         ChatInputField,
     }
 
+    TMP_Text _chatLog;
+    TMP_InputField _chatInputField;
+
     public override void Init()
     {
         Bind<TMP_Text>(typeof(Texts));
         Bind<TMP_InputField>(typeof(InputFields));
 
-        Get<TMP_Text>((int)Texts.ChatLog).text = string.Empty;
+        _chatLog = Get<TMP_Text>((int)Texts.ChatLog);
+        _chatLog.text = string.Empty;
+
+        _chatInputField = Get<TMP_InputField>((int)InputFields.ChatInputField);
+        _chatInputField.onSubmit.RemoveListener(SendInputMessage);
+        _chatInputField.onSubmit.AddListener(SendInputMessage);
+        //_chatInputField.onSelect.AddListener((s)=>print("onSelect"));
+        //_chatInputField.onDeselect.AddListener((s) => print("onDeselect"));
 
         Managers.Input.KeyAction -= CheckInput;
         Managers.Input.KeyAction += CheckInput;
@@ -31,36 +44,38 @@ public class UI_Chat : UI_Base
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            ToggleActive();
+            ToggleInputField();
         }
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         Init();
     }
 
-    private void ToggleActive()
+    private void ToggleInputField()
     {
-        //Get<TMP_InputField>((int)InputFields.ChatInputField).ActivateInputField();
-
-        TMP_InputField inputField = Get<TMP_InputField>((int)InputFields.ChatInputField);
-        print(inputField.isFocused);
-        if (inputField.isFocused)
+        if(EventSystem.current.currentSelectedGameObject == _chatInputField.gameObject)
         {
-            string message = inputField.text;
-            if(message != string.Empty)
-            {
-                Managers.LocalPlayer.PlayerController.SendChatMessage(message);
-                inputField.text = string.Empty;
-            }
+            EventSystem.current.SetSelectedGameObject(null);
         }
-        inputField.ActivateInputField();
+        else
+        {
+            _chatInputField.ActivateInputField();
+        }
+    }
+
+    public void SendInputMessage(string inputMessage)
+    {
+        if (inputMessage != string.Empty)
+        {
+            Managers.LocalPlayer.PlayerController.SendChatMessage(inputMessage);
+            _chatInputField.text = string.Empty;
+        }
     }
 
     public void AddMessage(string nickname, string message)
     {
-        Get<TMP_Text>((int)Texts.ChatLog).text += $"{nickname} : {message}\n";
+        _chatLog.text += $"{nickname} : {message}\n";
     }
 }
